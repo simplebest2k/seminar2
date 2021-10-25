87df155ecd9da79b0e53adf4eb08289108ee6112
simplebest2k <simplebest2k@naver.com>
2021-10-26 08:56:16 +0900

    TimeController time1 함수 추가

--- src/main/java/com/example/seminar1/controller/TimeController.java
+++ src/main/java/com/example/seminar1/controller/TimeController.java
@@ -1,7 +1,12 @@
 package com.example.seminar1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TimeController {
+    @GetMapping("/time1")
+    public String time1() {
+        return "time1";
+    }
 }
