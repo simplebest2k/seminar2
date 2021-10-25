51bab9c977cae020310268d1a27aa5ee2a308344
simplebest2k <simplebest2k@naver.com>
2021-10-26 08:33:43 +0900

    MainController 추가 main1 함수 추가

---
+++ src/main/java/com/example/seminar1/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.seminar1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/main1")
+    public String main1() {
+        return "main1";
+    }
+}
